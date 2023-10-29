33bb7017fe9f3beb61aa6bda73ac4641980720cf
mokwathedeveloper <moffatmokwa12@gmail.com>
2023-10-29 16:32:15 +0300

    completed Assigment

--- src/components/App.js
+++ src/components/App.js
@@ -9,7 +9,7 @@ function App() {
       <h1>{format(new Date(), "MMMM do yyyy, h:mm:ss a")}</h1>
       <p className="App-intro">
         In React apps, we write JSX - it looks like HTML, and uses a lot of HTML
-        syntax. JSX lets us include JavaScript functions right along with the
+        syntax. JSX lets us include JavaScript functions on right along with the
         HTML, and also allows us to add in components, which are separate,
         self-contained chunks of JSX.
       </p>
